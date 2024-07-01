6fe8053fda71d0604abc8175d40a5662d1631541
Alyssa Smith <smith.alyss@northeastern.edu>
2024-07-01 11:24:10 -0400

    correct jorts effect script

--- qt_jorts_effect.py
+++ qt_jorts_effect.py
@@ -4,24 +4,19 @@ import tweepy
 import datetime as dt
 import pytz
 
-jorts_tweets = pd.read_csv('./jorts_all_tweets.tsv', sep='\t')
+# The following code is meant to illustrate how a user can get time-bounded lists of follow events
+# using the Twitter V1 API. 
+# Note that rate limits for the following API make it difficult for this technique to scale.
 
-qt_authors = jorts_tweets[jorts_tweets['quoted'] != 'None'][['quoted_author_id', 'created_at', 'quoted_handle']]
-qt_authors = qt_authors[qt_authors['created_at'] != 'None']
-
-##############IMPORTANT################
-qt_authors = qt_authors[qt_authors.index % 2 == 0] # zhen use this line
-# qt_authors = qt_authors[qt_authors.index % 2 == 1] # mohit use this line
-qt_authors['created_at_ts'] = pd.to_datetime(qt_authors['created_at'])
-qt_authors = qt_authors.groupby('quoted_handle').min()
-qt_authors['quoted_handle'] = qt_authors.index
-
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAGu0TgEAAAAAhbwt8wcI2%2B7HTT8DTUiutbOM%2FZw%3DCVjxqMZ8eDZ7Zo5vFFs9Gdi1gc3ktbwJDg1KbCsABYbmVNODFd"
+bearer_token = 'YOUR BEARER TOKEN HERE'
 auth = tweepy.OAuth2BearerHandler(bearer_token)
 # some dreadful global scope to avoid passing `api` around to every function
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 def parse_cursor(c: int) -> dt.datetime:
+    # Credits to Stefan Mccabe; turns a Javascript cursor from Twitter into a Python unix timestamp 
+    # returns a datetime object
+    # our inspiration can be found at the link below (yes, really)
     # https://popzazzle.blogspot.com/2019/11/how-to-find-out-when-someone-followed-you-on-twitter.html
     if c == -1:
         return dt.datetime.now()
@@ -39,6 +34,8 @@ def parse_cursor(c: int) -> dt.datetime:
     # we now have a dt.timedelta of e days.
 
 def dt_to_cursor(ts: dt.datetime) -> int:
+    # turns a Python unix timestamp (datetime object) into a Javascript Twitter cursor
+    # Again, credit to Stefan Mccabe for writing the code that does this.
     utc_ts = ts.astimezone(pytz.UTC)
     utc_ts_delta = utc_ts - dt.datetime(2007, 3, 9, 7, 51, 0, 0, pytz.UTC) # timedelta of e days
     utc_unix_days = utc_ts_delta.days
@@ -49,19 +46,8 @@ def dt_to_cursor(ts: dt.datetime) -> int:
 
     return c
 
-day_unix_nanoseconds = 24 * 60 * 60 * (10 ** 9)
-def jorts_effect(author, screen_name, ts, api):
-    cursor = dt_to_cursor(ts)
-    before = cursor - (7 * day_unix_nanoseconds)
-    after = cursor + (7 * day_unix_nanoseconds)
-    # cursor_now = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=-1)
-    cursor_before = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=before)
-    cursor_cursor = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=cursor)
-    cursor_after = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=after)
-    return cursor_before, cursor_cursor, cursor_after
-# cursor_now, cursor_before, cursor_cursor, cursor_after = jorts_effect(48921931, 'mauraloran', ts, api)
-
 def find_list_alignment(past_list, future_list):
+    # Given two lists that have some overlap point, return the index at which future_list overlaps into past_list.
     idx = 0
     if len(past_list) == 0:
         if len(future_list) == 0:
@@ -79,24 +65,28 @@ def find_list_alignment(past_list, future_list):
     # but before future_list's boundary.
     return idx
 
-with open('alyssa_jorts_qt_effect.csv', 'a') as f:
-    for d in qt_authors.to_dict(orient="records"):
-        try:
-            cursor_before, cursor_cursor, cursor_after = jorts_effect(
-                d['quoted_author_id'], d['quoted_handle'], d['created_at_ts'], api)
-        except Exception as e:
-            print(e)
-            print(d['quoted_handle'])
-            continue
-        between_tweet_and_a_week_after = find_list_alignment(cursor_cursor[0], cursor_after[0])
-        between_a_week_before_tweet_and_tweet = find_list_alignment(cursor_before[0], cursor_cursor[0])
-        res = [
-            d['quoted_author_id'],
-            d['quoted_handle'],
-            d['created_at'],
-            between_tweet_and_a_week_after,
-            between_a_week_before_tweet_and_tweet,
-        ]
-        print(','.join([str(r) for r in res]))
-        f.write(','.join([str(r) for r in res]))
-        f.write('\n')
+day_unix_nanoseconds = 24 * 60 * 60 * (10 ** 9)
+def pull_followers_week_before_after(author: int, screen_name: str, ts: datetime.datetime, api:tweepy.API) -> tuple:
+    """
+    Get all users who followed an account a week before/after a timestamp ts.
+    In order to do this, we need:
+    - the users who followed after "before" (7 days before the TS)
+    - the users who followed after the cursor (at the TS) -- we will use these to eliminate overlap 
+    - the users who followed after "after" (7 days after the TS). 
+    
+    """
+    cursor = dt_to_cursor(ts)
+    before = cursor - (7 * day_unix_nanoseconds)
+    after = cursor + (7 * day_unix_nanoseconds)
+    # cursor_now = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=-1)
+    cursor_before = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=before)
+    cursor_cursor = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=cursor)
+    cursor_after = api.get_follower_ids(user_id=author, screen_name=screen_name, cursor=after)
+
+    between_tweet_and_a_week_after = find_list_alignment(cursor_cursor[0], cursor_after[0])
+    between_a_week_before_tweet_and_tweet = find_list_alignment(cursor_before[0], cursor_cursor[0])
+
+    return cursor_before[:between_a_week_before_tweet_and_tweet], cursor_cursor[:between_tweet_and_a_week_after]
+    return cursor_before, cursor_cursor, cursor_after
+
+
